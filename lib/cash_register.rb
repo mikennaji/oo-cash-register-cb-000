c691dd4890b40b14830bf376d845b50f9ba276a3
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-26 19:59:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,6 +2,8 @@ class CashRegister
 
 attr_accessor :total
 
+@@all= []
+
 def initialize(age = 0)
     @total = 0
     @discount= age
@@ -14,20 +16,25 @@ end
 def add_item(title,price,quantity=1)
  @quantity = quantity
   self.total += price * @quantity
+  @@all << title
+end
+
+def items 
+
+  @@all
 end
 
 def apply_discount
 
 
-      discount_percentage = (self.discount).to_f/100
-      discount =  (self.total*discount_percentage)
-      total_changed = self.total - discount
-      self.total = total_changed
-      if self.discount == 0
-        return "There is no discount to apply."
-      else
-        return"After the discount, the total comes to $#{self.total.to_i}."
-      end
+discount_percentage = (self.discount).to_f/100
+discount =  (self.total*discount_percentage)
+self.total = total_changed
+ if self.discount == 0
+    return "There is no discount to apply."
+ else
+    return"After the discount, the total comes to $#{self.total.to_i}."
+ end
 
 end
 
