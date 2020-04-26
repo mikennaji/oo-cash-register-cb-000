9b551331966d4f737babecb1e42b72b7462debf2
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-26 22:34:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ def initialize(age = 0)
     @total = 0
     @discount= age
     @titles = []
-    @transaction = []   #### if you add an array to track variablas when initialized it can be used to track anything added whilst the instatnce is being operated on with methods liek add
+    @transaction = []   #### if you add an array to track variables when initialized it can be used to track anything added whilst the instatnce is being operated on with methods liek add
 end
 
 def discount
