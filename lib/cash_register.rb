101b0ea9fffb21f2c8bf9bb7c1eac51970bc094c
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-26 19:27:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,7 @@ def apply_discount
   discount =  (self.total*discount_percentage)
   total_changed = self.total - discount
   self.total = total_changed
-
+  puts "After the discount, the total comes to #{self.total}"
 
 end
 
