2c3092ed9f9ad52a3160374bdec45cab0f84c94e
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-26 19:14:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,9 +19,9 @@ end
 def apply_discount
   new_register = CashRegister.new(self.discount)
   discount_percentage = (self.discount/100)
-  discount = (new_register.total*discount_percentage)
-  total_changed = new_register.total - discount
-  new_register.total = total_changed
+  discount = (self.total*discount_percentage)
+  total_changed = self.total - discount
+  self.total = total_changed
 
 
 end
