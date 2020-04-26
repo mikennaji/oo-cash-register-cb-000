6f3335b24d716c544df14e7a34f8d13b09da5588
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-26 20:07:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,15 +19,15 @@ end
 def apply_discount
 
 
-      discount_percentage = (self.discount).to_f/100
-      discount =  (self.total*discount_percentage)
-      total_changed = self.total - discount
-      self.total = total_changed
-      if self.discount == 0
-        return "There is no discount to apply."
-      else
-        return"After the discount, the total comes to $#{self.total.to_i}."
-      end
+ discount_percentage = (self.discount).to_f/100
+ discount =  (self.total*discount_percentage)
+ total_changed = self.total - discount
+ self.total = total_changed
+ if self.discount == 0
+   return "There is no discount to apply."
+ else
+   return"After the discount, the total comes to $#{self.total.to_i}."
+ end
 
 end
 
